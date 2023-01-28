672cfa6c6c068bc30bcda11d18b5b938f9f5d508
Akoseng <jameshii26@gmail.com>
2023-01-28 07:46:01 +0000

    fixing the promblem on the description after refresh

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
         // save data in local storage
         localStorage.setItem(time, event);
-    })
+    });
    
     function timeBlockColor() {
         //get current time in hour
@@ -40,29 +40,20 @@ $(document).ready(function () {
         })
     }
     
+    
 
     // get items from local storage
-    //$("#hour9 .description").val(localStorage.getItem("hour9"));
-    //$("#hour10 .description").val(localStorage.getItem("hour10"));
-    //$("#hour11 .description").val(localStorage.getItem("hour11"));
-    //$("#hour12 .description").val(localStorage.getItem("hour12"));
-    //$("#hour13 .description").val(localStorage.getItem("hour13"));
-    //$("#hour14 .description").val(localStorage.getItem("hour14"));
-    //$("#hour15 .description").val(localStorage.getItem("hour15"));
-    //$("#hour16 .description").val(localStorage.getItem("hour16"));
-    //$("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     timeBlockColor();
-
+    
 })
 
-function planner () {
-    $(".hour").each(function() {
-        let currentEvent = localStorage.getItem(currentTime);
-        if (currentEvent !== null) {
-            $(this).siblings(".event").val(currentEvent);
-        }
-    });
-}
-
-planner();
