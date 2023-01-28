6aa8b829451e47f117b1207728782784fcbdfd27
Akoseng <jameshii26@gmail.com>
2023-01-28 07:09:27 +0000

    add script.js with moment.js

---
+++ assets/JS/script.js
@@ -0,0 +1,68 @@
+// Display today's day and date on top
+let currentDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+    // click save to store the data
+    $(".saveBtn").on("click", function () {
+        let event = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+
+        // save data in local storage
+        localStorage.setItem(time, event);
+    })
+   
+    function timeBlockColor() {
+        //get current time in hour
+        let currentTime = moment().hour();
+
+        // 
+        $(".time-block").each(function () {
+            let blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // changing backgroud color to indicate if the event is future/present/past
+            if (currentTime > blockTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (currentTime === blockTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+    
+
+    // get items from local storage
+    //$("#hour9 .description").val(localStorage.getItem("hour9"));
+    //$("#hour10 .description").val(localStorage.getItem("hour10"));
+    //$("#hour11 .description").val(localStorage.getItem("hour11"));
+    //$("#hour12 .description").val(localStorage.getItem("hour12"));
+    //$("#hour13 .description").val(localStorage.getItem("hour13"));
+    //$("#hour14 .description").val(localStorage.getItem("hour14"));
+    //$("#hour15 .description").val(localStorage.getItem("hour15"));
+    //$("#hour16 .description").val(localStorage.getItem("hour16"));
+    //$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    timeBlockColor();
+
+})
+
+function planner () {
+    $(".hour").each(function() {
+        let currentEvent = localStorage.getItem(currentTime);
+        if (currentEvent !== null) {
+            $(this).siblings(".event").val(currentEvent);
+        }
+    });
+}
+
+planner();
